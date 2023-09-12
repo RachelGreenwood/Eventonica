6d7ad1d03a2ecdca0b3f9c9eff9d38978256c0a6
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-12 00:01:04 -0500

    Sets up front-end

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,9 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
