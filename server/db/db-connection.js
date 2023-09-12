0b577bd2142ed366771fffbaa8ecb3c4fd3e1050
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-11 23:42:50 -0500

    Connects to database

--- server/db/db-connection.js
+++ server/db/db-connection.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_URL,
   ssl: (process.env.DATABASE_SSL != "false") && {
     rejectUnauthorized: false
   }
