234db532f9de293ccd6c50a7a7563a3177a09bf2
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-12 15:49:24 -0500

    Shows datetime and adds description

--- client/src/components/event.jsx
+++ client/src/components/event.jsx
@@ -6,10 +6,11 @@ const EventCard = (props) => {
         <Card style={{ width: '18rem' }}>
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Date: {!props.time ? "TBD" : null}</Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">Date: {props.time ? props.time : "TBD"}</Card.Subtitle>
                 <Card.Text>
                     Location: {props.location}
                 </Card.Text>
+                <Card.Text>Description: {props.description}</Card.Text>
             </Card.Body>
         </Card>
     )
